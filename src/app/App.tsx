1d17d146f91ac9053fd13bcca2fa7e543d966032
danaksel <68808355+danaksel@users.noreply.github.com>
2026-07-01 14:01:33 +0200

    Hardcode browser theme color

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -450,7 +450,7 @@ export default function App() {
       metaThemeColor.setAttribute('name', 'theme-color');
       document.head.appendChild(metaThemeColor);
     }
-    metaThemeColor.setAttribute('content', darkMode ? '#2d3a21' : '#e8ede3');
+    metaThemeColor.setAttribute('content', '#2d3a21');
 
     // Set apple-mobile-web-app-status-bar-style
     let appleStatusBar = document.querySelector('meta[name="apple-mobile-web-app-status-bar-style"]');
